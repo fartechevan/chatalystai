e2aa8763816fef1a8132faa6399aeefe6916ad94
evanbeh <evan@airasia.com>
2025-04-12 12:34:04 +0800

    clear conversations fix

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -14,7 +14,7 @@ export default function Dashboard() {
         <DashboardSidebar />
         <main className="flex-1 pt-8 px-8 overflow-auto relative">
           {/* Removed logout button container */}
-          <div className="container">
+          <div>
             <Outlet />
           </div>
         </main>
